0b560fd63484b433163f404b0ed392c0a3a9fa7a
JB <jaruwak@kkumail.com>
2021-12-25 13:58:15 +0700

    add popup

---
+++ js/popup.js
@@ -0,0 +1,6 @@
+function toBlur() {
+    var blurB = document.getElementById('blur')
+    blurB.classList.toggle('active')
+    var pop = document.getElementById('popup')
+    pop.classList.toggle('active')
+}
