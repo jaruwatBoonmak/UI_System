d85ead7472b77afe09ade18b3d1c1fe893b5bc19
jaruwatBoonmak <jaruwak@kkumail.com>
2022-09-16 16:10:23 +0700

    new

--- js/my_chart.js
+++ js/my_chart.js
@@ -1,33 +1,33 @@
-const ctx = document.getElementById('myChart').getContext('2d');
+//const ctx = document.getElementById('myChart').getContext('2d');
 const ctx2 = document.getElementById('myChart2').getContext('2d');
 
-const myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive:true
-    }
-});
+// const myChart = new Chart(ctx, {
+//     type: 'doughnut',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         responsive:true
+//     }
+// });
 
 
-myChart = new Chart(ctx2, {
+const myChart = new Chart(ctx2, {
     type: 'bar',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
+            label: 'จำนวน',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 1)',
