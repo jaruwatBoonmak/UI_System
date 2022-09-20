ab3b2bf127f516c1c45211976bbb20c0fe571aac
jaruwatBoonmak <jaruwak@kkumail.com>
2022-09-20 15:08:02 +0700

    topbar add

--- js/popup.js
+++ js/popup.js
@@ -4,3 +4,21 @@ function toBlur() {
     var pop = document.getElementById('popup')
     pop.classList.toggle('active')
 }
+
+
+
+var box  = document.getElementById('box');
+var down = false;
+
+
+function toggleNotifi(){
+	if (down) {
+		box.style.height  = '0px';
+		box.style.opacity = 0;
+		down = false;
+	}else {
+		box.style.height  = '150px';
+		box.style.opacity = 1;
+		down = true;
+	}
+}
